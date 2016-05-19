00e10641ca80d472542fe8d6feafce65c002ab4d
Guillaume Potier <cobalt2760@gmail.com>
2016-05-19 17:24:25 +0200

    - updated blog uk url

--- build/app.js
+++ build/app.js
@@ -366,7 +366,7 @@
 	              { className: 't-caption t-caption--copy' },
 	              React.createElement(
 	                'a',
-	                { href: __('http://wisembly.com/en/blog/2016/05/03/uk-survey-wisembly-censuswide-meeting-cost-2016?utm=meetingcalculator&utc=meetingcalculator'), target: '_blank' },
+	                { href: __('http://wisembly.com/en/blog/2016/05/19/uk-survey-wisembly-censuswide-meeting-cost-2016'), target: '_blank' },
 	                __('Wisembly 2016 UK Survey'),
 	                React.createElement('br', null),
 	                __('conducted by Censuswide')
@@ -20266,7 +20266,7 @@
 	    'Attending meetings': 'Participant à des réunions',
 	    'Wisembly 2016 UK Survey': 'Étude France Wisembly 2016',
 	    'conducted by Censuswide': 'réalisée par l\'IFOP',
-	    'http://wisembly.com/en/blog/2016/05/03/uk-survey-wisembly-censuswide-meeting-cost-2016?utm=meetingcalculator&utc=meetingcalculator': 'http://wisembly.com/blog/2016/03/30/la-reunion-de-demain-sera-productive-quels-formats-privilegier-resultats-du-barometre-annuel-de-wisembly-en-partenariat-avec-lifop',
+	    'http://wisembly.com/en/blog/2016/05/19/uk-survey-wisembly-censuswide-meeting-cost-2016': 'http://wisembly.com/blog/2016/03/30/la-reunion-de-demain-sera-productive-quels-formats-privilegier-resultats-du-barometre-annuel-de-wisembly-en-partenariat-avec-lifop',
 	    'Contact us': 'Contactez nous',
 	    'What does this mean? eg: Based on our research, on average, Operations teams runs more unproductive meetings (41%) than any other department. A 20-people Operations team consequently looses on average in the UK 1,859 working hours and £30,347 per year during these improductive meetings.': 'Que signifie ce résultat ? Sur la base de notre baromètre annuel, les cadres dans les départements Finances sont les plus inefficaces en réunion (43%). Une équipe financière de 20 collaborateurs perd annuellement, en moyenne, en France, 1618 heures de travail et 40 952 euros au cours de réunions improductives.',
 	    'What does this mean? eg: Based on our research, on average, Aeronautics & Defense runs more unproductive meetings (67%) than any other industry. A 500-people Aeronautics & Defense company consequently looses on average in the UK 54,188 working hours and £868,649 per year during these improductive meetings.': 'Que signifie ce résultat ? Sur la base de notre baromètre annuel, les cadres dans le secteur de la Grande Distribution sont les plus inefficaces en réunion (47%). Une entreprise dans ce secteur de 500 collaborateurs perd annuellement, en moyenne, en France, 5 658 heures de travail et 136 799 euros au cours de réunions improductives.'
